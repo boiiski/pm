7542fec81a524be1525ec19883820f922f1bfc90
boiiski <gameboi@live.com>
2020-09-18 10:42:21 +1000

    ghost movement functionality, currenttly movement is randomized

--- app.js
+++ app.js
@@ -159,4 +159,39 @@ document.addEventListener('DOMContentLoaded', () => {
       squares[ghost.currentIndex].classList.add(ghost.className)
       squares[ghost.currentIndex].classList.add('ghost')
       })
+
+      ghosts.forEach(ghost => moveGhost(ghost))
+
+      function moveGhost(ghost) {
+        const directions =  [-1, +1, width, -width]
+        let direction = directions[Math.floor(Math.random() * directions.length)]
+    
+        ghost.timerId = setInterval(function() {
+          //if the next squre your ghost is going to go to does not have a ghost and does not have a wall
+          if  (!squares[ghost.currentIndex + direction].classList.contains('ghost') &&
+            !squares[ghost.currentIndex + direction].classList.contains('wall') ) {
+              //remove the ghosts classes
+              squares[ghost.currentIndex].classList.remove(ghost.className)
+              squares[ghost.currentIndex].classList.remove('ghost', 'scared-ghost')
+              //move into that space
+              ghost.currentIndex += direction
+              squares[ghost.currentIndex].classList.add(ghost.className, 'ghost')
+          //else find a new random direction ot go in
+          } else direction = directions[Math.floor(Math.random() * directions.length)]
+    
+          //if the ghost is currently scared
+          if (ghost.isScared) {
+            squares[ghost.currentIndex].classList.add('scared-ghost')
+          }
+    
+          //if the ghost is currently scared and pacman is on it
+          if(ghost.isScared && squares[ghost.currentIndex].classList.contains('pac-man')) {
+            squares[ghost.currentIndex].classList.remove(ghost.className, 'ghost', 'scared-ghost')
+            ghost.currentIndex = ghost.startIndex
+            score +=100
+            squares[ghost.currentIndex].classList.add(ghost.className, 'ghost')
+          }
+        checkForGameOver()
+        }, ghost.speed)
+      }
 })
